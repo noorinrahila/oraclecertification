0aa19962523f0706badf82286697d78cd77e2c72
KC <KC@KC-PC>
2017-06-20 21:14:51 +0530

    new commit

--- book/src/main/java/com/book/book/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.book.book.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.book.book.model.User;
-
-public interface UserRepository extends JpaRepository<User,Integer>{
-
-}
