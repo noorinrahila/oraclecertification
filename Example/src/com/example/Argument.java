d64f147b8f5b0d91a33a6b886d2c99278c314f75
KC <KC@KC-PC>
2017-06-21 19:52:38 +0530

    new commit

--- Example/src/com/example/Argument.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Argument {
-	public static void main(String[] args){
-		System.out.println(args[0]);
-	}
-
-}
---
+++ Oracle/src/com/example/ComparatorValues.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class ComparatorValues {
+public static void main(String[] args){
+	java.util.List<Integer> list = new  java.util.ArrayList<Integer>();
+	list.add(10);
+	for(int x:list){
+		System.out.println(x);
+		break;
+		
+	}
+}
+}
